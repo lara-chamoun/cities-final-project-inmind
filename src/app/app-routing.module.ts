1ab6dca174df004a1632ff84c8cb637e13e04794
larachamoun <lara11shamoun@gmail.com>
2023-08-13 12:36:30 +0300

    Modules Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignuppageComponent } from './signuppage/signuppage.component';
-import { LoginpageComponent } from './loginpage/loginpage.component';
+import { SignuppageComponent } from './auth-module/signuppage/signuppage.component';
+import { LoginpageComponent } from './auth-module/loginpage/loginpage.component';
+import { CountriesPageComponent } from './countries-module/countries-page/countries-page.component';
 
 const routes: Routes = [
   { path: '', component: LoginpageComponent }, //default route
-  { path: 'signup', component: SignuppageComponent }
+  { path: 'signup', component: SignuppageComponent },
+  { path: 'countries', component: CountriesPageComponent }
+
+
 ];
 
 @NgModule({
---
+++ src/app/countries-module/countries-page/countries-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countries-page',
+  templateUrl: './countries-page.component.html',
+  styleUrls: ['./countries-page.component.scss']
+})
+export class CountriesPageComponent {
+
+}
