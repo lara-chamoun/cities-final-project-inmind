0f06ccf85547d18aeb235d159e6115ffe334c0c0
larachamoun <lara11shamoun@gmail.com>
2023-08-13 12:51:21 +0300

    DescriptionPage

--- src/app/countries-module/countries-module.module.ts
+++ src/app/countries-module/countries-module.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CountriesListComponent } from './countries-list/countries-list.component';
 import { CountriesPageComponent } from './countries-page/countries-page.component';
+import { DescriptionPageComponent } from './description-page/description-page.component';
 
 
 
 @NgModule({
   declarations: [
     CountriesListComponent,
-    CountriesPageComponent
+    CountriesPageComponent,
+    DescriptionPageComponent
   ],
   imports: [
     CommonModule
